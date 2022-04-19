b46c977379622ea1e2c7f1969ba63cdbf4f16608
Luis(刘思林) <luis.liu@amberaigroup.com>
2022-04-19 13:01:49 +0800

    log4j2 and springboot for log4j2

---
+++ slf4j-log4j2/src/main/java/com/yangtzelsl/Log4j2Test.java
@@ -0,0 +1,10 @@
+package com.yangtzelsl;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Log4j2Test {
+    public static void main(String[] args) {
+        log.info("hello world.");
+    }
+}
--- springboot-log/src/main/java/com/yangtzelsl/SpringbootLogApplication.java
+++ springboot-log/src/main/java/com/yangtzelsl/SpringbootLogApplication.java
@@ -1,12 +1,17 @@
 package com.yangtzelsl;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class SpringbootLogApplication {
 
     public static void main(String[] args) {
+
+        // 日志用法
+        log.info("这是我的测试方法，不需要定义Logger属性，即可输出日志文件");
 
         SpringApplication.run(SpringbootLogApplication.class, args);
     }
